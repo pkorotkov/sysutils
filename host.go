e68efbae8392e211d660d69e7786f9d1e4adbd58
Pavel Korotkov <pkorotkov@gmail.com>
2015-02-13 23:41:06 +0300

    Fix error message

--- host.go
+++ host.go
@@ -13,7 +13,7 @@ import (
 func GetSystemStartTime() (time.Time, error) {
     var ut C.ULONGLONG = C.GetTickCount64()
     if ut == 0 {
-        return time.Time{}, fmt.Errorf("failed to retrieve uptime value")
+        return time.Time{}, fmt.Errorf("C.GetTickCount64: failed to retrieve uptime value")
     }
     return time.Now().Add(-time.Duration(ut) * time.Millisecond), nil
 }
